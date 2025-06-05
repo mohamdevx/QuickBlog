52d0037640714df90f0119703a2790306acaad9a
Mohammad <mbalyallpuri@gmail.com>
2025-06-05 15:37:46 +0500

    32m Navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
 
 const App = () => {
   return (
-    <div>
-    
-    </div>
-  )
-}
+<div>
+      {/* Wrap all routes in <Routes> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+      </div>
+  );
+};
 
-export default App
+export default App;
