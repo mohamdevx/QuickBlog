db0481f2f9d8e53163f70435b637360cf686d826
Mohammad <mbalyallpuri@gmail.com>
2025-06-10 22:50:16 +0500

    start admin controller func 4:00

---
+++ server/controllers/adminController.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export  const adminLogin=async()=>{
+    try {
+        const {email,password}=req.body;
+
+        if(email !==process.env.ADMIN_EMAIL || password !== process.env.
+            ADMIN_PASSWORD){
+                return res.json({success:false,message: "Invalid credentials"})
+        }
+        const token=jwt.sign({email},process.env.JWT_SECRET )
+        res.json({success:true})
+        
+    } catch (error) {
+        res.json({success:false, message: error.message});
+        
+    }
+}
